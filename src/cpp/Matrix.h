68a83a2c5ecea379e6b6f1372b6dbc85971e0fc1
Pgarrett <philip.garrett@mulesoft.com>
2022-10-18 09:23:58 -0300

    Using eigenLib

--- src/cpp/Matrix.h
+++ src/cpp/Matrix.h
@@ -10,23 +10,32 @@
 #include "Eigenpair.h"
 #include "string"
 
+#include "Eigen/Sparse"
+
 using namespace std;
+using namespace Eigen;
 
 typedef vector<vector<double>> matrix;
 
+struct EP2 {
+    double eigenvalue;
+    VectorXd eigenvector;
+};
+
 namespace MatrixOperator {
-matrix read(string filename);
+
+
+//matrix read(string filename);
+    SparseMatrix<double> read(string filename);
 void scaleMatrix(matrix &m, double scalar);
 void substract(matrix &a, const matrix &b);
 matrix outerProduct(vector<double> &u, vector<double> &v);
 double innerProduct(vector<double> &u, vector<double> &v);
-matrix similarityMatrix(const matrix &a);
+matrix similarityMatrix(const Matrix<double, Dynamic, Dynamic, RowMajor> &a);
 void deleteMaxEigenValue(matrix &m, double a, vector<double> v);
 vector<double> multiplyMatrixByVector(const matrix &m, const vector<double> &v);
-eigenPair powerMethod(const matrix &m, int iterations, double epsilon,
-                      int lambda);
-vector<eigenPair> deflationMethod(const matrix &m, int iterations,
-                                  double epsilon);
+eigenPair power_iteration(const Matrix<double, Dynamic, Dynamic, RowMajor> &X, unsigned iterations, double epsilon);
+vector<eigenPair> deflationMethod(const Matrix<double, Dynamic, Dynamic, RowMajor> &m, int iterations, double epsilon);
 } // namespace MatrixOperator
 
 namespace MatrixPrinter {
