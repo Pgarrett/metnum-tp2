e8938eb53b00f0d66197154c696fca7404e7a711
Pgarrett <philip.garrett@mulesoft.com>
2022-10-21 21:40:10 -0300

    building adjacency matrix

--- src/main/python/config.py
+++ src/main/python/config.py
@@ -2,3 +2,4 @@ compareToleranceEpsilon=1e-1
 iterations=1e6
 powerMethodEpsilon=1e-1
 debug=False
+uValues=range(2,10)
--- src/main/python/exercise3.py
+++ src/main/python/exercise3.py
@@ -1,18 +1,37 @@
-def adjacencyByU(u):
-    # s = matrixBuilder.similarity()
-    # adj = cutByU(s, u)
+import config as cfg
+import matrixBuilder as mBuilder
 
-def flattenCompare(transformedFacebookEdgesFile):
-    # foreach u
-        # adjacencyByU(u)
+def adjacencyByU(similarity, u):
+    result = []
+    for row in similarity:
+        rowResult = []
+        for item in row:
+            if item > u:
+                rowResult.append(1)
+            else:
+                rowResult.append(0)
+        result.append(rowResult)
+    return result
+
+# def flattenCompare(similarity, transformedFacebookEdgesFile):
+#     for u in cfg.uValues:
+#         adj = adjacencyByU(similarity, u)
         # doCompare
         # plotter.generateUCutsForFlatten
 
-def eigenValueCompare(transformedFacebookEdgesFile):
+# def eigenValueCompare(transformedFacebookEdgesFile):
     # foreach u
         # adjacencyByU(u)
         # adjEigen = runTpFor(adj)
         # facebookEdgesEigen = runTpFor(transformedFacebookEdgesFile)
     # plotter.generateUCutsForEigenValues
 
-def chooseOptimumUValue():
+# def chooseOptimumUValue():
+
+def run():
+    similarity = mBuilder.buildSimilarityMatrix()
+    adj = adjacencyByU(similarity, 2)
+    print(adj)
+
+
+run()
